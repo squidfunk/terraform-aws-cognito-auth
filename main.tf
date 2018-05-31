d8ccd0e2ec0f43cfdf4a4fd7701b7676f57730bb
squidfunk <martin.donath@squidfunk.com>
2018-05-31 12:55:21 +0200

    Moved authentication endpoint to own module

--- main.tf
+++ main.tf
@@ -231,86 +231,35 @@ resource "aws_api_gateway_deployment" "_" {
   }
 
   depends_on = [
-    "aws_api_gateway_integration._",
+    "module.authenticate",
     "module.register",
     "module.reset",
   ]
 }
 
 # -----------------------------------------------------------------------------
-# Resources: API Gateway: POST /
-# -----------------------------------------------------------------------------
-
-# aws_api_gateway_method._
-resource "aws_api_gateway_method" "_" {
-  rest_api_id   = "${aws_api_gateway_rest_api._.id}"
-  resource_id   = "${aws_api_gateway_rest_api._.root_resource_id}"
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-# aws_api_gateway_integration._
-resource "aws_api_gateway_integration" "_" {
-  rest_api_id = "${aws_api_gateway_rest_api._.id}"
-  resource_id = "${aws_api_gateway_rest_api._.root_resource_id}"
-  http_method = "${aws_api_gateway_method._.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-
-  uri = "arn:aws:apigateway:${
-      var.region
-    }:lambda:path/2015-03-31/functions/${
-      aws_lambda_function._.arn
-    }/invocations"
-}
-
-# -----------------------------------------------------------------------------
-# Resources: Lambda
-# -----------------------------------------------------------------------------
-
-# aws_lambda_function._
-resource "aws_lambda_function" "_" {
-  function_name = "${var.namespace}"
-  role          = "${aws_iam_role.lambda.arn}"
-  runtime       = "nodejs8.10"
-  filename      = "${path.module}/lambda/dist.zip"
-  handler       = "handlers/index.post"
-  timeout       = 10
-
-  source_code_hash = "${
-    base64sha256(file("${path.module}/lambda/dist.zip"))
-  }"
-
-  environment {
-    variables = {
-      COGNITO_USER_POOL        = "${aws_cognito_user_pool._.id}"
-      COGNITO_USER_POOL_CLIENT = "${aws_cognito_user_pool_client._.id}"
-      DYNAMODB_TABLE           = "${aws_dynamodb_table._.name}"
-    }
-  }
-}
-
-# aws_lambda_permission._
-resource "aws_lambda_permission" "_" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function._.arn}"
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "arn:aws:execute-api:${
-      var.region
-    }:${
-      data.aws_caller_identity._.account_id
-    }:${
-      aws_api_gateway_rest_api._.id
-    }/*/${
-      aws_api_gateway_method._.http_method
-    }/"
-}
-
-# -----------------------------------------------------------------------------
 # Modules
+# -----------------------------------------------------------------------------
+
+# module.authenticate
+module "authenticate" {
+  source = "./modules/authenticate"
+
+  namespace = "${var.namespace}"
+  region    = "${var.region}"
+
+  api_id          = "${aws_api_gateway_rest_api._.id}"
+  api_resource_id = "${aws_api_gateway_rest_api._.root_resource_id}"
+
+  cognito_user_pool        = "${aws_cognito_user_pool._.id}"
+  cognito_user_pool_client = "${aws_cognito_user_pool_client._.id}"
+
+  dynamodb_table = "${aws_dynamodb_table._.name}"
+
+  lambda_role_arn = "${aws_iam_role.lambda.arn}"
+  lambda_filename = "${path.module}/lambda/dist.zip"
+}
+
 # -----------------------------------------------------------------------------
 
 # module.register
