cc2753bdae4a777d3909667d1ea05309b6f39bf5
squidfunk <martin.donath@squidfunk.com>
2018-09-07 17:15:22 +0200

    Added /check endpoint for token validation

---
+++ modules/api/modules/check/main.tf
@@ -0,0 +1,83 @@
+# Copyright (c) 2018 Martin Donath <martin.donath@squidfunk.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+# -----------------------------------------------------------------------------
+# Data: General
+# -----------------------------------------------------------------------------
+
+# data.aws_caller_identity._
+data "aws_caller_identity" "_" {}
+
+# -----------------------------------------------------------------------------
+# Resources: API Gateway: POST /check
+# -----------------------------------------------------------------------------
+
+# aws_api_gateway_resource._
+resource "aws_api_gateway_resource" "_" {
+  rest_api_id = "${var.api_id}"
+  parent_id   = "${var.api_resource_id}"
+  path_part   = "check"
+}
+
+# aws_api_gateway_method._
+resource "aws_api_gateway_method" "_" {
+  rest_api_id   = "${var.api_id}"
+  resource_id   = "${aws_api_gateway_resource._.id}"
+  http_method   = "POST"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = "${var.api_authorizer_id}"
+}
+
+# aws_api_gateway_integration._
+resource "aws_api_gateway_integration" "_" {
+  rest_api_id = "${var.api_id}"
+  resource_id = "${aws_api_gateway_resource._.id}"
+  http_method = "${aws_api_gateway_method._.http_method}"
+
+  type = "MOCK"
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 204 }"
+  }
+}
+
+# aws_api_gateway_integration_response._
+resource "aws_api_gateway_integration_response" "_" {
+  rest_api_id = "${var.api_id}"
+  resource_id = "${aws_api_gateway_resource._.id}"
+  http_method = "${aws_api_gateway_method._.http_method}"
+  status_code = 204
+
+  depends_on = [
+    "aws_api_gateway_integration._",
+  ]
+}
+
+# aws_api_gateway_method_response._
+resource "aws_api_gateway_method_response" "_" {
+  rest_api_id = "${var.api_id}"
+  resource_id = "${aws_api_gateway_resource._.id}"
+  http_method = "${aws_api_gateway_method._.http_method}"
+  status_code = 204
+
+  depends_on = [
+    "aws_api_gateway_method._",
+  ]
+}
