024420b3dd147c00a4a71d8f57b33c0712015864
squidfunk <martin.donath@squidfunk.com>
2019-01-01 18:45:30 +0100

    Updated copyright year

--- README.md
+++ README.md
@@ -302,7 +302,7 @@ future.
 
 __MIT License__
 
-Copyright (c) 2018 Martin Donath
+Copyright (c) 2018-2019 Martin Donath
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
--- modules/web/app/tests/mocks/enhancers/with-form-submit.ts
+++ modules/web/app/tests/mocks/enhancers/with-form-submit.ts
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2018 Martin Donath <martin.donath@squidfunk.com>
+ * Copyright (c) 2018-2019 Martin Donath <martin.donath@squidfunk.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
