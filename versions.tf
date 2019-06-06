f4eebc04df6a4d10402b86c85953702c39528f43
squidfunk <martin.donath@squidfunk.com>
2019-06-06 19:32:22 +0200

    Upgraded Terraform and Terraform AWS Provider to latest versions

--- versions.tf
+++ versions.tf
@@ -18,22 +18,6 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-# -----------------------------------------------------------------------------
-# Variables: Cloudfront
-# -----------------------------------------------------------------------------
-
-# output.cloudfront_distribution_domain_name
-output "cloudfront_distribution_domain_name" {
-  value = "${local.enabled == 1
-    ? aws_cloudfront_distribution._.0.domain_name
-    : ""
-  }"
-}
-
-# output.cloudfront_distribution_hosted_zone_id
-output "cloudfront_distribution_hosted_zone_id" {
-  value = "${local.enabled == 1
-    ? aws_cloudfront_distribution._.0.hosted_zone_id
-    : ""
-  }"
+terraform {
+  required_version = ">= 0.12"
 }
