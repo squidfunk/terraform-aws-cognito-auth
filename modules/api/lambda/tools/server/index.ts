e6a4d20a0efd5d463e9b8b72aacb44ec8bb4e7b9
squidfunk <martin.donath@squidfunk.com>
2020-10-20 18:09:08 +0200

    Updated web module

--- modules/api/lambda/tools/server/index.ts
+++ modules/api/lambda/tools/server/index.ts
@@ -20,6 +20,8 @@
  * IN THE SOFTWARE.
  */
 
+// tslint:disable no-console
+
 import "dotenv/config"
 
 import * as express from "express"
@@ -41,7 +43,6 @@ import { middleware } from "./middleware"
       console.log(`API server listening on http://localhost:9091`)
     })
   } catch (err) {
-    // tslint:disable-next-line no-console
     console.log(err)
   }
 })()
